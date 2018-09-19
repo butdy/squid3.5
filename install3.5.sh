195fe5258bb2591e71d5c5a4248aa70227b09a21
butdy <butom.gogo@gmail.com>
2018-09-19 16:37:38 +0700

    nulis syntaks pertama kali

---
+++ install3.5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p 'Masukan User Linux Anda: ' userver
+read -sp 'password Linux Anda: ' passserver 
+echo "Ini masih setengah Otomatis" 
+echo -e "selamat datang $userver di program installasi\n"
+echo -n "Masukan Nama Anda :"
+read nama
+echo "Hello $nama"
+echo "goodluck $nama"
+echo "oh iya ... !sebelumnya clone dari github git clone git@github.com:butdy/squid3.5.git"
